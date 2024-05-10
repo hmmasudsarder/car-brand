adb58908a1cfa39610b67dd226815fb7c993f168
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-05-10 15:21:26 +0600

    card poblem solv

--- src/Pages/BrandCard/ShowCard.jsx
+++ src/Pages/BrandCard/ShowCard.jsx
@@ -1,39 +1,34 @@
-
 import { Link } from "react-router-dom";
 
-const ShowCard = ({ category}) => {
+const ShowCard = ({ category }) => {
   // console.log(category)
   const { _id, photo, name, price, rating, category: cat } = category;
-  
-//   console.log(category);
-//   console.log(cat);
+
+  //   console.log(category);
+  //   console.log(cat);
   return (
     <div className="w-[70vh]">
-    
-      <div className="card card-compact bg-base-100 shadow-xl">
+      <div className="card card-compact bg-base-100 shadow-xl h-full mt-20 ">
         <figure>
-          <img className=""
-            src={photo}
-            alt="Shoes"
-          />
+          <img className="w-full h-auto" src={photo} alt="Shoes" />
         </figure>
-        <div className="card-body">
+        <div className="card-body flex flex-col flex-grow">
           <h2 className="card-title">Name: {name}</h2>
           <h2 className="card-title">Brand Name: {cat}</h2>
           <h2 className="card-title">Price:$ {price}</h2>
           <h2 className="card-title">Rating: {rating}</h2>
-         
-          <div className="flex item-center justify-between ">
-          <div className="">
-            <Link to={`/updateProduct/${_id}`}>
-            <button className="btn  bg-accent">Update</button>
-            </Link>
-          </div>
-          <div className="">
-            <Link to={`/showDetails/${_id}`}>
-            <button className="btn btn-secondary">Details</button>
-            </Link>
-          </div>
+
+          <div className="flex item-center justify-between flex-grow">
+            <div className="">
+              <Link to={`/updateProduct/${_id}`}>
+                <button className="btn  bg-accent">Update</button>
+              </Link>
+            </div>
+            <div className="">
+              <Link to={`/showDetails/${_id}`}>
+                <button className="btn btn-secondary">Details</button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,23 +6,22 @@ import BrandCard from "./BrandCard/BrandCard";
 import Banner from "../Components/Navbar/Banner";
 import TwoExtraSection from "../Components/ExtraSection/TwoExtraSection";
 
-
 const Home = () => {
-    const brands = useLoaderData();
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Banner/>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 m-24">
-            {
-                brands.map(brand => <BrandCard key={brand._id} brand={brand}></BrandCard>)
-            }
-            </div>
-            <ExtraSection></ExtraSection>
-            <TwoExtraSection></TwoExtraSection>
-            <Footer/>
-        </div>
-    );
+  const brands = useLoaderData();
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Banner />
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 m-24">
+        {brands.map((brand) => (
+          <BrandCard key={brand._id} brand={brand}></BrandCard>
+        ))}
+      </div>
+      <ExtraSection></ExtraSection>
+      <TwoExtraSection></TwoExtraSection>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
